5b3129eafe7d8adeedd823011f4607b5315b5e27
HHHuser1 <134743133+HHHuser1@users.noreply.github.com>
2023-06-11 17:03:38 -0400

    Added website folders and files
    Created proper website folder structure and added index.php, services.php, style.css and all the images used on the website to their assigned folders

---
+++ index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>FINAL PROJECT </title>
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+
+<link rel="stylesheet" href="./assets/css/style.css">
+
+</head>
+<body>
+
+	<header class="page-header">
+		<div class="container">
+			<div class="header-flex">
+				
+				<div class="logo"><a href=""><img src="./assets/images/logo.png" alt="LOGO here"></a></div>
+
+
+				<nav>
+					<ul class="header-menu">
+						<li><a href="index.php">home</a></li>
+						<li><a href="">about</a></li>
+						<li><a href="services.php">services</a></li>
+						<li><a href="">portfolio</a></li>
+						<li><a href="">contact</a></li>
+					</ul>
+				</nav>
+
+
+			</div>
+		</div>
+	</header>
+
+
+
+
+	<section class="banner">
+		<div class="textbox">
+			<h1>we are creative</h1>
+			<h1>agency</h1>
+		</div>
+	</section>
+
+	<section class="about">
+		<div class="container">
+			
+			<div class="about-flex">
+				<div class="left">
+					<h3>Who we are</h3>
+					<div class="bar"></div>
+					<h2>we are <b>design agency </b>that bring your ideas alive</h2>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa corrupti aperiam vitae nulla perspiciatis omnis, nihil ab ad modi asperiores, voluptatibus soluta odit ullam, sit.</p>
+					<br>
+					<p>Lorem, ipsum dolor sit amet consectetur, adipisicing elit elit el. ispa. blablabla</p>
+					<a href="" class="btn">learn more</a></div>
+				<div class="right"></div>
+			</div>
+		</div>
+	</section>
+
+
+	<section class="fourcard">
+		<div class="container">
+			<h2>latest work</h2>
+			<div class="cardflex">
+				<div class="cardleft">
+					<div class="card">		
+					<div class="topleft-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+				</div>
+					<div class="card">
+					<div class="bottomleft-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+					</div>
+				</div>
+
+				<div class="cardright">
+					<div class="card">
+					<div class="topright-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+					</div>
+					<div class="card">
+					<div class="bottomright-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+					</div>
+				</div>
+						
+			</div>
+			<a href="#" class="fourcard-btn">more portfolio</a>
+		</div>
+
+	</section>
+
+
+	<section class="featured-service">
+		<div class="container">
+			<div class="service-flex">
+				<div class="serviceleft">
+					<h3>featured service</h3>
+					<div class="bar"></div>
+					<h2>The <b>services </b>that we are providing</h2>
+					<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsa corrupti aperiam vitae nulla perspiciatis omnis, nihil ab ad modi asperiores, voluptatibus soluta odit ullam, sit.</p>
+					<br>
+					<p>Lorem, ipsum dolor sit amet consectetur, adipisicing elit elit el. blablabla</p>
+				</div>
+				<div class="serviceright">
+
+						<div class="service-card-flex">
+
+							<div class="service-card">card1
+							<div class="icon"></div>
+							<h3>branding</h3>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Quis, nihil. Perspiciatis ad atque sunt cum eos odio.</p>
+							</div>
+
+							<div class="service-card">card2
+							<div class="icon"></div>
+							<h3>web design</h3>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Quis, nihil. Perspiciatis ad atque sunt cum eos odio.</p>
+							</div>
+
+							<div class="service-card">card3
+							<div class="icon"></div>
+							<h3>app design</h3>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Quis, nihil. Perspiciatis ad atque sunt cum eos odio.</p>
+							</div>
+
+							<div class="service-card">card4
+							<div class="icon"></div>
+							<h3>start up</h3>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing, elit. Quis, nihil. Perspiciatis ad atque sunt cum eos odio.</p>
+							</div>
+
+						</div>
+				</div>
+					
+			</div>
+		</div>
+
+	</section>
+
+	<section class="cta">
+		<div class="container">
+			<h4>do you have an awesome project in mind?</h4>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing, elit. Possimus enim recusandae ratione assumen</p>
+			<a href="" class="cta-btn">get in touch</a>
+		</div>
+	</section>
+
+
+
+	
+</body>
+</html>
---
+++ services.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+
+	<header class="page-header">
+		<div class="container">
+			<div class="header-flex">
+				
+				<div class="logo"><a href=""><img src="./assets/images/logo.png" alt="LOGO here"></a></div>
+
+
+				<nav>
+					<ul class="header-menu">
+						<li><a href="index.php">home</a></li>
+						<li><a href="">about</a></li>
+						<li><a href="services.php">services</a></li>
+						<li><a href="">portfolio</a></li>
+						<li><a href="">contact</a></li>
+					</ul>
+				</nav>
+
+
+			</div>
+		</div>
+	</header>
+
+    
+    <section class="fourcard">
+		<div class="container">
+			<h2>latest work</h2>
+			<div class="cardflex">
+				<div class="cardleft">
+					<div class="card">		
+					<div class="topleft-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+				</div>
+					<div class="card">
+					<div class="bottomleft-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+					</div>
+				</div>
+
+				<div class="cardright">
+					<div class="card">
+					<div class="topright-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+					</div>
+					<div class="card">
+					<div class="bottomright-img">
+						<div class="grey-box">
+							<h4>work name here</h4>
+							<p>category here</p>
+						</div>
+					</div>
+					</div>
+				</div>
+						
+			</div>
+			<a href="#" class="fourcard-btn">more portfolio</a>
+		</div>
+
+	</section>
+
+
+
+
+
+</body>
+</html>
